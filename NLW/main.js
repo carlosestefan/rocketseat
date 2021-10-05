d2e1a6628a3042c1d92e2d4cae4d57fbbc6f9a33
carlosestefan <79671191+carlosestefan@users.noreply.github.com>
2021-10-05 04:18:49 -0300

    nav menu e sessões

---
+++ NLW/main.js
@@ -0,0 +1,31 @@
+//ABRE E FECHA MENU
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for(const element of toggle) {
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show');
+  })
+}
+
+// LINKS ESCONDER MENU
+const links = document.querySelectorAll('nav ul li a');
+
+for(const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show');
+  })
+}
+
+// SOMBRA NO HEADER
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll');
+  }else {
+    header.classList.remove('scroll');
+  }
+})
